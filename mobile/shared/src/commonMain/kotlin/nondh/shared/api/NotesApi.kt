1e314534f37c48db5f8c01996176ada2da1d0865
Semal Patel <semalPatel@users.noreply.github.com>
2026-02-26 21:32:33 -0800

    fix(mobile): add ktor header import and bump heap

--- mobile/shared/src/commonMain/kotlin/nondh/shared/api/NotesApi.kt
+++ mobile/shared/src/commonMain/kotlin/nondh/shared/api/NotesApi.kt
@@ -3,6 +3,7 @@ package nondh.shared.api
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.request.url
